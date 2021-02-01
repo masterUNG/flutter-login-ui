a377d3295a00acdacedcda792f8df64f551efe66
masterUNG <phrombutr@gmail.com>
2021-02-01 16:54:21 +0700

    update

--- lib/Screens/login/login.dart
+++ lib/Screens/login/login.dart
@@ -12,112 +12,128 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: EdgeInsets.symmetric(horizontal: 40),
-              child: Text(
-                "LOGIN",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xFF2661FA),
-                  fontSize: 36
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ),
-
+            buildTextLogin(),
             SizedBox(height: size.height * 0.03),
-
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: "Username"
-                ),
-              ),
-            ),
-
+            buildUser(),
             SizedBox(height: size.height * 0.03),
-
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.symmetric(horizontal: 40),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: "Password"
-                ),
-                obscureText: true,
-              ),
-            ),
-
-            Container(
-              alignment: Alignment.centerRight,
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: Text(
-                "Forgot your password?",
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Color(0XFF2661FA)
-                ),
-              ),
-            ),
-
+            buildPassword(),
+            buildForgotPassword(),
             SizedBox(height: size.height * 0.05),
-
-            Container(
-              alignment: Alignment.centerRight,
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: RaisedButton(
-                onPressed: () {},
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
-                textColor: Colors.white,
-                padding: const EdgeInsets.all(0),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 50.0,
-                  width: size.width * 0.5,
-                  decoration: new BoxDecoration(
-                    borderRadius: BorderRadius.circular(80.0),
-                    gradient: new LinearGradient(
-                      colors: [
-                        Color.fromARGB(255, 255, 136, 34),
-                        Color.fromARGB(255, 255, 177, 41)
-                      ]
-                    )
-                  ),
-                  padding: const EdgeInsets.all(0),
-                  child: Text(
-                    "LOGIN",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                ),
-              ),
-            ),
-
-            Container(
-              alignment: Alignment.centerRight,
-              margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-              child: GestureDetector(
-                onTap: () => {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen()))
-                },
-                child: Text(
-                  "Don't Have an Account? Sign up",
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xFF2661FA)
-                  ),
-                ),
-              ),
-            )
+            buildButtonLogin(size),
+            buildSignUp(context)
           ],
         ),
       ),
     );
+  }
+
+  Container buildSignUp(BuildContext context) {
+    return Container(
+            alignment: Alignment.centerRight,
+            margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+            child: GestureDetector(
+              onTap: () => {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => RegisterScreen()))
+              },
+              child: Text(
+                "Don't Have an Account? Sign up",
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF2661FA)
+                ),
+              ),
+            ),
+          );
+  }
+
+  Container buildButtonLogin(Size size) {
+    return Container(
+            alignment: Alignment.centerRight,
+            margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+            child: RaisedButton(
+              onPressed: () {},
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(80.0)),
+              textColor: Colors.white,
+              padding: const EdgeInsets.all(0),
+              child: Container(
+                alignment: Alignment.center,
+                height: 50.0,
+                width: size.width * 0.5,
+                decoration: new BoxDecoration(
+                  borderRadius: BorderRadius.circular(80.0),
+                  gradient: new LinearGradient(
+                    colors: [
+                      Color.fromARGB(255, 255, 136, 34),
+                      Color.fromARGB(255, 255, 177, 41)
+                    ]
+                  )
+                ),
+                padding: const EdgeInsets.all(0),
+                child: Text(
+                  "LOGIN",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+            ),
+          );
+  }
+
+  Container buildForgotPassword() {
+    return Container(
+            alignment: Alignment.centerRight,
+            margin: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+            child: Text(
+              "Forgot your password?",
+              style: TextStyle(
+                fontSize: 12,
+                color: Color(0XFF2661FA)
+              ),
+            ),
+          );
+  }
+
+  Container buildPassword() {
+    return Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(horizontal: 40),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Password"
+              ),
+              obscureText: true,
+            ),
+          );
+  }
+
+  Container buildUser() {
+    return Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(horizontal: 40),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: "Username"
+              ),
+            ),
+          );
+  }
+
+  Container buildTextLogin() {
+    return Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Text(
+              "LOGIN",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF2661FA),
+                fontSize: 36
+              ),
+              textAlign: TextAlign.left,
+            ),
+          );
   }
 }
